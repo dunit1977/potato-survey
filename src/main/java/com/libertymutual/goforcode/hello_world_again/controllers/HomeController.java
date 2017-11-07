b05a5e74ba1f8f4a5b60db7bbf282c7aee89a484
Doug George <douglas.george@libertymutual.com>
2017-11-07 14:09:14 -0500

    Initial commit of the potato survey application.

---
+++ src/main/java/com/libertymutual/goforcode/hello_world_again/controllers/HomeController.java
@@ -0,0 +1,48 @@
+package com.libertymutual.goforcode.hello_world_again.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	private int russetCount;
+	private int goldenCount;
+	private int sweetCount;
+
+	@RequestMapping("/")
+	public String defaultPage() {
+		return "potato";
+	}
+
+	@RequestMapping("/twice-baked")
+	public ModelAndView twiceBaked() {
+		ModelAndView carbs = new ModelAndView();
+		carbs.setViewName("spuds");
+		carbs.addObject("variety", "fingerling");
+		carbs.addObject("temperature", 425);
+		return carbs;
+	}
+
+	@RequestMapping("/survey")
+	public ModelAndView survey(String answer) {
+		ModelAndView survey = new ModelAndView();
+		survey.setViewName("results");
+		survey.addObject("userResponse", answer);
+
+		if (answer.equals("Sweet")) {
+			sweetCount = sweetCount + 1;
+		}
+		if (answer.equals("Russet")) {
+			russetCount = russetCount + 1;
+		}
+		if (answer.equals("Golden")) {
+			goldenCount = goldenCount + 1;
+		}
+        survey.addObject("sweetCount", sweetCount);
+        survey.addObject("russetCount", russetCount);
+        survey.addObject("goldenCount", goldenCount);
+		return survey;
+	}
+}
